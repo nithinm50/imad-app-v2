b704777c79b570f69c2623b624c1322910170cd6
nithinm50 <nithinm50@gmail.com>
2017-02-05 15:12:58 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -41,6 +41,12 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+    var counter = 0;
+app.get('/counter', function (req, res) {
+    counter = counter + 1;
+    res.send(counter);
+});
+
 function createTemplate(data) {
     var title = data.title;
     var date = data.date;
