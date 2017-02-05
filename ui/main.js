ce8e8899992684469d791bcb6d47ba4c44c243a2
nithinm50 <nithinm50@gmail.com>
2017-02-05 16:05:50 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,2 +1,24 @@
 console.log('Loaded!');
-var name= document.getElementId("name");
+var nameInput= document.getElementId("name");
+var name = nameInput.value();
+var submit = document.getElementById("submit_btn");
+submit.onclick = function(){
+    var request = new XMLHttpRequest();
+    
+    request.onreadystatechange = function(){
+        if(request.readyState === XMLHttpRequest.DONE){
+            if(request.status === 200){
+                var names = request.responseText;
+                names = JSON.parse(names);
+                var list = '';
+                for(var i=0;i<name.length; i++){
+                    list +='<li>'+ names[i] +'</li>';     
+                }
+                var ul = document.getElementById("namelist");
+                ul.innerHTML=list;
+            }
+        }
+    };
+    request.open('GET', "http://nithinm50.imad.hasura-app.io/submit-name?name="+ name, true);
+    request.send(null);
+};
