0c5424a550f570b7632b7678ff272bd6e2af4850
nithinm50 <nithinm50@gmail.com>
2017-03-04 14:18:44 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -110,7 +110,7 @@ app.get('/submit-name', function(req, res){
 });
 
 app.get('/article/:articleName', function (req, res) {
-    pool.query("select * from article where title = &1", [req.params.articleName], function(err, result){
+    pool.query("select * from article where title = $1", [req.params.articleName], function(err, result){
         if(err){
              res.status(500).send(err.toString());
          } else {
