98fdddf2712d4abeea47434fe434b93c6573d711
nithinm50 <nithinm50@gmail.com>
2017-03-04 12:01:21 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -54,9 +54,9 @@ var pool = new Pool(config);
 app.get('/test-db', function (req, res){
     pool.query('select * from test', function(err,result){
          if(err){
-             res.status(500).send(err.toStringify());
+             res.status(500).send(err.toString());
          } else {
-             res.send(JSON.stringify(result.rows));
+             res.send(JSON.stringify(result));
          }
     });
 });
