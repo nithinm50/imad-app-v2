295b974d9b0fd0b2b8f2932251b6293eb9a02407
nithinm50 <nithinm50@gmail.com>
2017-03-12 18:15:07 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var morgan = require('morgan');
 var path = require('path');
 var Pool = require('pg').Pool;
 var crypto = require('crypto');
-
+var bodyParser = require('body-Parser');
 var config = {
     user: 'nithinm50',
     database: 'nithinm50',
@@ -14,6 +14,7 @@ var config = {
 
 var app = express();
 app.use(morgan('combined'));
+app.use(bodyParser.json());
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
@@ -80,6 +81,21 @@ function hash(input, salt){
 app.get('/hash/:input', function(req, res){
 	var hashedString = hash(req.params.input, 'this-is-some-random-string');
 	res.send(hashedString);
+});
+
+app.post('/create-user', function(req, res){
+    
+    var username = req.body.username;
+    var password = req.body.password;
+	var salt = crypto.randomBytes(128).toString('hex');
+	var dbString = hash(password, salt);
+	pool.query('insert into "user" (username, password) values ($1, $2)', [username, dbString], function(err, result){
+	if(err){
+		res.status(500).send(err.toString());
+		} else {
+		res.send("user Sucessfully Created"+ username);
+		}
+    });
 });
 
 var names =[];
