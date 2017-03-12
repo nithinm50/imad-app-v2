ec75628b238288384e1f1e2360bf496342f22a0f
nithinm50 <nithinm50@gmail.com>
2017-03-12 16:22:52 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -78,7 +78,7 @@ function hash(input, salt){
 }
 
 app.get('/hash/:input', function(req, res){
-	var hashedString = hash(req.params.input, 'this-is-some-random-string');
+	var hashedString = "hello";
 	res.send(hashedString);
 });
 
