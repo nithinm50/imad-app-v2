a3ec3eec908fe0742f62bc4a2e7b94bd2593dc81
nithinm50 <nithinm50@gmail.com>
2017-03-15 09:24:40 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -74,14 +74,16 @@ function createTemplate(data) {
     return htmlTemplate;
 }
 
-function hash(input, salt){
-	var hashed = crypto.pbkdf2Sync( input, salt, 10000, 512, 'sha512');
-	return hashed.toString('hex');
+function hash(input , salt){
+    //How do we create a hash
+    var hashed = crypto.pbkdf2Sync(input ,salt ,10000 ,512 ,'sha512');
+    return ['pbkdf2Sync','10000', salt, hashed.toString('hex')].join('$');
 }
 
-app.get('/hash/:input', function(req, res){
-	var hashedString = hash(req.params.input, 'this-is-some-random-string');
-	res.send(hashedString);
+app.get('/hash/:input', function (req, res){
+    var hashedString = hash(req.params.input , 'this-is-some-random-string');
+    res.send(hashedString);
+    
 });
 
 app.post('/create-user' ,function (req ,res){
