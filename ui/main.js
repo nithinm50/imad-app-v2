a2fc4a3988ca04a2f4fe7bf0f713459939b7ef75
nithinm50 <nithinm50@gmail.com>
2017-03-16 08:47:16 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -23,7 +23,32 @@ submit.onclick = function(){
     request.open('GET', "http://nithinm50.imad.hasura-app.io/submit-name?name="+ name, true);
     request.send(null);
 };
-
+// login form
+var submit = document.getElementById('submit_btn');
+submit.onclick = function(){
+    console.log("inside");
+    var request = new XMLHttpRequest();
+    
+    request.onreadystatechange = function(){
+        if(request.readyState === XMLHttpRequest.DONE){
+            if(request.status === 200){
+                console.log("login sucessful");
+                alert("login sucessful");
+            } else if(request.status === 403) {
+                alert("incorrect username/password");
+            } else if(request.status === 500) {
+                alert("something went wrong");
+            }
+        }
+    };
+    var username= document.getElementById('username').value;
+    var password= document.getElementById('password').value;
+    console.log('username');
+    console.log('password');
+    request.open('POST', "http://nithinm50.imad.hasura-app.io/login", true);
+    request.setRequestHeader('Content-Type','application/json');
+    request.send(JSON.stringify({username:username, password:password}));
+};
 // console.log("inside");
 //     var names =['name1','name2', 'name3'];
 //     var list = '';
