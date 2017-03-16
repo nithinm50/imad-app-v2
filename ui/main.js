d1209086ddeb20824ec8ba36d329453d0375415a
nithinm50 <nithinm50@gmail.com>
2017-03-16 08:58:45 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -43,7 +43,8 @@ submit.onclick = function(){
     };
     var username= document.getElementById('username').value;
     var password= document.getElementById('password').value;
-    console.log("username");
+    var username1= document.getElementById('username').value;
+    console.log("username1");
     console.log('password');
     request.open('POST', "http://nithinm50.imad.hasura-app.io/login", true);
     request.setRequestHeader('Content-Type','application/json');
