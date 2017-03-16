9bc2415206afaa2d3bd7cfafa91e72e52dab1e7d
nithinm50 <nithinm50@gmail.com>
2017-03-16 08:49:24 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,28 +1,28 @@
-console.log('Loaded!');
-var submit = document.getElementById('submit_btn');
-submit.onclick = function(){
-    console.log("inside");
-    var request = new XMLHttpRequest();
+// console.log('Loaded!');
+// var submit = document.getElementById('submit_btn');
+// submit.onclick = function(){
+//     console.log("inside");
+//     var request = new XMLHttpRequest();
     
-    request.onreadystatechange = function(){
-        if(request.readyState === XMLHttpRequest.DONE){
-            if(request.status === 200){
-                var names = request.responseText;
-                names = JSON.parse(names);
-                var list = '';
-                for(var i=0;i<names.length; i++){
-                    list +='<li>'+ names[i] +'</li>';     
-                }
-    var ul = document.getElementById('namelist');
-    ul.innerHTML = list;
-            }
-        }
-    };
-    var nameinput= document.getElementById('name');
-    var name = nameinput.value;
-    request.open('GET', "http://nithinm50.imad.hasura-app.io/submit-name?name="+ name, true);
-    request.send(null);
-};
+//     request.onreadystatechange = function(){
+//         if(request.readyState === XMLHttpRequest.DONE){
+//             if(request.status === 200){
+//                 var names = request.responseText;
+//                 names = JSON.parse(names);
+//                 var list = '';
+//                 for(var i=0;i<names.length; i++){
+//                     list +='<li>'+ names[i] +'</li>';     
+//                 }
+//     var ul = document.getElementById('namelist');
+//     ul.innerHTML = list;
+//             }
+//         }
+//     };
+//     var nameinput= document.getElementById('name');
+//     var name = nameinput.value;
+//     request.open('GET', "http://nithinm50.imad.hasura-app.io/submit-name?name="+ name, true);
+//     request.send(null);
+// };
 // login form
 var submit = document.getElementById('submit_btn1');
 submit.onclick = function(){
@@ -47,7 +47,7 @@ submit.onclick = function(){
     console.log('password');
     request.open('POST', "http://nithinm50.imad.hasura-app.io/login", true);
     request.setRequestHeader('Content-Type','application/json');
-    request.send(JSON.stringify({username:username, password:password}));
+    request.send(JSON.stringify({username: username, password: password}));
 };
 // console.log("inside");
 //     var names =['name1','name2', 'name3'];
